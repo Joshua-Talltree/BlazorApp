8d18edf80c2764ea5eecb2091b856dd35f4caca4
Joshua-Talltree <73854373+Joshua-Talltree@users.noreply.github.com>
2021-04-25 23:22:14 -0500

    new Blazor w/ mongo DB training app

---
+++ Shared/Models/EmployeeDBContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorWithMango.Shared.Models
+{
+    public class EmployeeDBContext
+    {
+    }
+}
